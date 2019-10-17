f79ceaa0d9dfb67f3c7efb7847de58b8d1edbe0d
Gabriell Oliveira <gabrielloliveira097@gmail.com>
2019-10-17 15:35:59 -0300

    adicionado funcionalidade de cadastro de usuários

--- hardcore/website/views.py
+++ hardcore/website/views.py
@@ -5,6 +5,7 @@ from django.http import HttpResponseRedirect
 from django.contrib.auth.models import User
 from django.shortcuts import render
 from django.urls import reverse
+import random
 
 
 # Create your views here.
@@ -13,7 +14,7 @@ def index(request):
 
 def login_user(request):
     if request.user.is_authenticated:
-        return HttpResponseRedirect("/core/")
+        return HttpResponseRedirect(reverse('core:index'))
 
     if 'next' in request.GET:
         request.session['redirect'] = request.GET['next']
@@ -40,11 +41,53 @@ def login_user(request):
         except User.DoesNotExist:
             return render(request, 'website/login.html', {'message': 'E-mail ou senha incorretos.', 'class': 'has-text-danger'})    
         
-    if 'mensagem' in request.session:
-        mensagem = request.session['mensagem']
-        classe = request.session['classe']
-        del request.session['mensagem']
-        del request.session['classe']
-        return render(request, 'website/login.html', {'message': mensagem, 'class': classe})
+    if 'message' in request.session:
+        message = request.session['message']
+        class_name = request.session['class']
+        del request.session['message']
+        del request.session['class']
+        return render(request, 'website/login.html', {'message': message, 'class': class_name})
 
     return render(request, 'website/login.html')
+
+def signup(request):
+    if request.user.is_authenticated:
+        return HttpResponseRedirect(reverse('core:index'))
+
+    if request.POST:
+        name = request.POST['name']
+        email = request.POST['email']
+        password = request.POST['password']
+        course = request.POST['course']
+        cpf = request.POST['cpf']
+        birth_date = request.POST['birth-date']
+        sex = request.POST['sex']
+
+        choices = 'qwertyuiopasdfghjklzxcvbnm1234567890' 
+        username = ''
+        for i in range(18):
+            username += random.choice(choices)
+        
+        while User.objects.filter(username=username).exists():
+            username = ''
+            for i in range(18):
+                username += random.choice(choices)
+
+        if User.objects.filter(email=email).exists():
+            return render(request, 'website/signup.html', {'message': 'E-mail já cadastrado.', 'class': 'has-text-danger'})
+
+        user = User.objects.create_user(username=username, email=email, password=password)
+        user.save()
+        user.profile.full_name = name
+        user.profile.course_name = course
+        user.profile.cpf = cpf
+        user.profile.birth_date = birth_date
+        user.profile.sex = sex
+        user.save()
+
+        request.session['message'] = 'Usuário cadastrado com sucesso. Faça Login'
+        request.session['class'] = 'has-text-success'
+
+        return HttpResponseRedirect(reverse('website:login'))
+    
+    return render(request, 'website/signup.html')
