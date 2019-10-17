84bac09a9d0a832b6e7c04f95e1f3de27c55327c
Gabriell Oliveira <gabrielloliveira097@gmail.com>
2019-10-17 14:19:02 -0300

    adicionado tabela de perfil de usuário

--- hardcore/core/models.py
+++ hardcore/core/models.py
@@ -1,3 +1,32 @@
+from django.db.models.signals import post_save
+from django.contrib.auth.models import User
+from django.dispatch import receiver
+from datetime import date
 from django.db import models
 
 # Create your models here.
+class Profile(models.Model):
+    SEX_CHOICES = [
+        ('F', 'Feminino'),
+        ('M', 'Masculino'),
+    ]
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    course_name = models.CharField(max_length=255, blank=False, null=False)
+    cpf = models.CharField(max_length=14, blank=False, null=False)
+    date_birth = models.DateField(blank=False, null=False)
+    sex = models.CharField(max_length=1, blank=False, null=False)
+
+    @property
+    def age(self):
+        now = date.today()
+        return now.year - self.date_birth.year - ((now.month, now.day) < (self.date_birth.month, self.date_birth.day))
+
+
+@receiver(post_save, sender=User)
+def create_profile(sender, instance, created, **kwargs):
+    if created:
+        Profile.objects.create(user=instance)
+
+@receiver(post_save, sender=User)
+def save_profile(sender, instance, **kwargs):
+    instance.profile.save()
