8254af9bfff37bb80f522044be103de1db6a0d45
Gabriell Oliveira <gabrielloliveira097@gmail.com>
2019-10-17 14:30:45 -0300

    refatorado tela de login

--- hardcore/website/views.py
+++ hardcore/website/views.py
@@ -1,5 +1,13 @@
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required
+from django.views.decorators.http import require_POST
+from django.http import HttpResponseRedirect
+from django.contrib.auth.models import User
 from django.shortcuts import render
 
 # Create your views here.
 def index(request):
     return render(request, 'website/index.html')
+
+def login(request):
+    return render(request, 'website/login.html')
