0ef815f450f72706be5bb5ea95cf1cd1b16a2d0b
lucasbezerra26 <lucasbezerra527@gmail.com>
2019-10-17 14:55:15 -0300

    Models da aaa

--- hardcore/aaa/models.py
+++ hardcore/aaa/models.py
@@ -5,10 +5,13 @@ from django.conf import settings
 from django.utils import timezone
 
 class Aaa(models.Model):
-    name = models.CharField(max_length=200)
+    name = models.CharField(max_length=255, blank=False, null=False)
     created_data = models.DateTimeField(default=timezone.now)
-    logo = models.CharField(max_length=200)
+    logo = models.CharField(max_length=255, blank=False, null=False)
     value_association = models.DecimalField(decimal_places=2, max_digits=8)
+
+    def qtd_members(self):
+        pass
 
     def __str__(self):
         return self.name
