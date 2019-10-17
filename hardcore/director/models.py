3b8977fc1ed9d5ca8c2055189cd55364ce803929
lucasbezerra26 <lucasbezerra527@gmail.com>
2019-10-17 17:11:51 -0300

    Models do director

--- hardcore/director/models.py
+++ hardcore/director/models.py
@@ -4,8 +4,17 @@ from django.db import models
 from django.conf import settings
 from django.utils import timezone
 from django.contrib.auth.models import User
+from aaa.models import AAA 
 
-class Director():
-    name = models.OneToOneField(User, on_delete=models.CASCADE)
+class Director(models.Model):
+    user = models.ForeignKey(User, on_delete = models.CASCADE)
     office = models.CharField('Cargo',max_length=255, blank=False, null=False)
+    athletic = models.ForeignKey(AAA, on_delete = models.CASCADE)
+
+    class Meta:
+        verbose_name = 'Director'
+        verbose_name_plural = 'Directors'
+
+    def __str__(self):
+        return self.user.profile.full_name
     
