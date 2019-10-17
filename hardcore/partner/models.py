628d9418191b3f47c7bf3bd20c9a3ad79da59d37
spbdlelis <samuellelis88@gmail.com>
2019-10-17 16:36:24 -0300

    Associação e Parceiro completos

--- hardcore/partner/models.py
+++ hardcore/partner/models.py
@@ -1,7 +1,14 @@
 from django.db import models
+from aaa.models import AAA 
 
 # Create your models here.
 class Partner(models.Model):
-    pass
+    name = models.CharField(max_length=50,blank = True, null = True)
+    logo = models.ImageField(blank = True, null = True)
+    description = models.TextField(blank = True, null = True)
+    athletic = models.ForeignKey(AAA,blank = True, null = True , on_delete = models.CASCADE)
+
+    def __str__(self):
+        return self.name
 
 
