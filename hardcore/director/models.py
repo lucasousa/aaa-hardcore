7fd9b0dc00a34227fc7bf7098294094d1dbafbf9
lucasbezerra26 <lucasbezerra527@gmail.com>
2019-10-17 16:40:17 -0300

    comecando o model do director

--- hardcore/director/models.py
+++ hardcore/director/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+from django.conf import settings
+from django.utils import timezone
+from django.contrib.auth.models import User
+
+class Director():
+    name = models.OneToOneField(User, on_delete=models.CASCADE)
+    office = models.CharField('Cargo',max_length=255, blank=False, null=False)
+    
