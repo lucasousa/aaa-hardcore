0578626b0736ae33ac2b9affd083ba698e048bdd
lucasbezerra26 <lucasbezerra527@gmail.com>
2019-10-17 14:29:33 -0300

    Comeaçãndo o models da aaa

--- hardcore/aaa/models.py
+++ hardcore/aaa/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+from django.conf import settings
+from django.utils import timezone
+
+class Aaa(models.Model):
+    name = models.CharField(max_length=200)
+    created_data = models.DateTimeField(default=timezone.now)
+    logo = models.CharField(max_length=200)
+    value_association = models.DecimalField(decimal_places=2, max_digits=8)
+
+    def __str__(self):
+        return self.name
