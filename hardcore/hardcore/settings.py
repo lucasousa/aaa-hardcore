18fc035b5567f8b188a0d1e360b081d63d840ca2
lucasbezerra26 <lucasbezerra527@gmail.com>
2019-10-17 13:31:35 -0300

    Adcionando a settings todos as app criadas

--- hardcore/hardcore/settings.py
+++ hardcore/hardcore/settings.py
@@ -62,6 +62,15 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+
+                #nossos
+                'aaa',
+                'association',
+                'core',
+                'director',
+                'notice',
+                'partner',
+                'product',
             ],
         },
     },
