e5055610ec51369a1906f88f7e8091e657ebb06c
Gabriell Oliveira <gabrielloliveira097@gmail.com>
2019-10-18 13:26:22 -0300

    adicionado template de cadatastro de parceiros

--- hardcore/partner/urls.py
+++ hardcore/partner/urls.py
@@ -5,4 +5,5 @@ app_name = 'partner'
 
 urlpatterns = [
     path('', views.index, name="index"),
+    path('add/', views.add, name="add"),
 ]
--- hardcore/partner/views.py
+++ hardcore/partner/views.py
@@ -3,7 +3,9 @@ from .models import Partner
 
 # Create your views here.
 
-
 def index(request):
     object_list = Partner.objects.all()
     return render(request, 'partner/manage-partner.html', {'objetos':object_list})
+
+def add(request):
+    return render(request, 'partner/add.html')
