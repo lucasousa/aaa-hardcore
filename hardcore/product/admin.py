7f48d94b724cab32b04e486b02097e0021cf4099
lucasousa <lucas103101@gmail.com>
2019-10-22 13:49:36 -0300

    Criando o Banco e Adicionando no Django Admin

--- hardcore/product/admin.py
+++ hardcore/product/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from product.models import Product
 
-# Register your models here.
+
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ['name', 'description', 'value', 'image']
+    actions = []
+
+admin.site.register(Product)
