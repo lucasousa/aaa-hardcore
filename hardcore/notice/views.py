91c0bb1508c2652236c4fbd7e82bc38bfb76a33c
lucasousa <lucas103101@gmail.com>
2019-10-22 13:14:08 -0300

    Adicionando as views

--- hardcore/notice/views.py
+++ hardcore/notice/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+from .models import Notice
 
-# Create your views here.
+def index(request):
+    object_list = Notice.objects.all() #Pegando todas as notícias do banco de dados
+    return render(request, 'notice/manage_notice.html', {'objetos':object_list})
+
+
+def new_notice(request):
+    if request.POST:
+        title = request.POST['title']
+        text = request.POST['text']
+        post.published_date = timezone.now()
+        notice = Notice.objects.create(title = title, text = text,  published_date = post.published_date)
+        notice.save()
+    return render(request, 'notice/new_notice.html')
