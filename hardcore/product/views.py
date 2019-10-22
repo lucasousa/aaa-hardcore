f0b0a3831639cf43f3d6a34307fbb42591c513cf
lucasbezerra26 <lucasbezerra527@gmail.com>
2019-10-22 16:38:17 -0300

    ajustando o template

--- hardcore/product/views.py
+++ hardcore/product/views.py
@@ -4,5 +4,4 @@ from .models import Product
 
 def product(request):
     object_list = Product.objects.all()
-    print('\n\n',object_list)
     return render(request, 'product/product.html', {'objetos':object_list})
