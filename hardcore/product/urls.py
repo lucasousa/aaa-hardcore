a09afbb71ae1af78c34bf7d2c2d33ef9f4493a56
lucasbezerra26 <lucasbezerra527@gmail.com>
2019-10-22 14:40:25 -0300

    crinado o arquivo urls de product

---
+++ hardcore/product/urls.py
@@ -0,0 +1,9 @@
+# from django.contrib import admin
+# from django.urls import path, include
+# from django.conf.urls.static import static
+# from django.conf import settings
+# from . import views
+
+# urlpatterns = [
+#     path('produtos/', views.product_list, name='product_list')
+# ]
