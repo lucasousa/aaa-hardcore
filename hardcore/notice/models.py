d79290856f80952744de8eb255dfd99294a8d8b1
lucasousa <lucas103101@gmail.com>
2019-10-22 15:52:49 -0300

    Criando uma slug

--- hardcore/notice/models.py
+++ hardcore/notice/models.py
@@ -4,6 +4,8 @@ from aaa.models import AAA
 from django.urls import reverse
 from django.utils.text import slugify
 
+from .utils import gerador_slug_unica
+from django.db.models.signals import pre_save
 
 class Notice(models.Model):
     title = models.CharField(blank=False, max_length=200)
@@ -20,7 +22,8 @@ class Notice(models.Model):
     def __str__(self):
         return self.title
 
-    def save(self, *args, **kwargs):
-        value = self.title
-        self.slug = slugify(value, allow_unicode=True)
-        super().save(*args, **kwargs)
+def slug_save(sender, instance, *args, **kwargs):
+    if not instance.slug:
+        instance.slug = gerador_slug_unica(instance, instance.title, instance.slug) 
+
+pre_save.connect(slug_save, sender=Notice)
