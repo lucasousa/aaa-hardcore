9faca298dbad977e8c85ebbfd0c217cfaf33c69f
lucasbezerra26 <lucasbezerra527@gmail.com>
2019-10-22 15:30:11 -0300

    fazendo a view de product

--- hardcore/product/urls.py
+++ hardcore/product/urls.py
@@ -1,9 +1,11 @@
-# from django.contrib import admin
-# from django.urls import path, include
-# from django.conf.urls.static import static
-# from django.conf import settings
-# from . import views
+from django.contrib import admin
+from django.urls import path, include
+from django.conf.urls.static import static
+from django.conf import settings
+from . import views
 
-# urlpatterns = [
-#     path('produtos/', views.product_list, name='product_list')
-# ]
+app_name = 'product'    
+
+urlpatterns = [
+    path('', views.product, name='product')
+]
