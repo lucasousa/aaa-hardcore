c2e39cacdf1ce69a98f7fc9bd2fc8a1c51da81a0
Gabriell Oliveira <gabrielloliveira097@gmail.com>
2019-12-06 16:34:13 -0300

    crud app director

--- hardcore/director/views.py
+++ hardcore/director/views.py
@@ -8,18 +8,53 @@ from django.urls import reverse
 import random
 from association.models import Association
 from .models import Director
+from aaa.models import AAA
 
 @login_required
 def manage(request):
     if not request.user.is_staff:
         return HttpResponseRedirect(reverse("core:index"))
-    
+
     users = Director.objects.all().order_by('user__profile__full_name')
+
+    if 'busca' in request.GET:
+        users = users.filter(user__profile__full_name__icontains=request.GET['busca'])
+
     return render(request, "director/manage.html", {'users': users})
 
 @login_required
 def add(request):
     if not request.user.is_staff:
         return HttpResponseRedirect(reverse("core:index"))
+
+    if request.POST:
+        user_cpf = request.POST['user'].split('| ')[1]
+        user = User.objects.get(profile__cpf=user_cpf)
+        user.is_superuser = True
+        user.save()
+        office = request.POST['office']
+
+        aaa = AAA.objects.get(id=1)
+        director = Director.objects.create(athletic=aaa, user=user, office=office)
+        director.save()
+        return HttpResponseRedirect(reverse("director:manage"))
+
+    users = User.objects.all()
     
-    return render(request, "director/add.html")
+    return render(request, "director/add.html", {'users': users})
+
+@login_required
+def delete(request, id):
+    if not request.user.is_staff:
+        return HttpResponseRedirect(reverse("core:index"))
+
+    director = Director.objects.get(id=id)
+    user = User.objects.get(id=director.user.id)
+
+    if not user.is_staff:
+        user.is_superuser = False
+        user.save()
+    
+    director.delete()
+
+    return HttpResponseRedirect(reverse("director:manage"))
