0527625fbe3aaa4fb7a284544b674e9462466e8a
Lucas Sousa <lucas103101@gmail.com>
2020-12-21 17:06:17 -0300

    Adicionado a secret key em um arquivo local pra proteger a aplicação

--- hardcore/hardcore/settings.py
+++ hardcore/hardcore/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '$73*25pb@4wdm(j$#m+v7s5usd@%ri^)(e)ho17qq)u4&_2px7'
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
